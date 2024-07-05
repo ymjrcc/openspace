245a121e6f174f896f0b43c07fa6ada6c91a8503
heyiming <ymjrcc@qq.com>
2024-07-05 10:14:45 +0800

    update Bank

--- W1D3-Bank/Bank.sol
+++ W1D3-Bank/Bank.sol
@@ -4,15 +4,9 @@ pragma solidity ^0.8.20;
 contract Bank {
     address public owner;
     mapping(address => uint256) public balances;
-    struct Contributor {
-        address addr;
-        uint256 amount;
-    }
-    // 定义数组记录所有存款者，方便排序
-    Contributor[] public contributors;
+    address[3] public top3;
 
     constructor() {
-        // 初始化管理员账户
         owner = msg.sender;
     }
 
@@ -22,19 +16,25 @@ contract Bank {
     }
 
     receive() external payable {
+        deposit();
+    }
+
+    function deposit() public payable{
+        require(msg.value > 0, "Deposit amount must be greater than 0");
         balances[msg.sender] += msg.value;
-        bool isExisting = false;
-        for (uint256 i = 0; i < contributors.length; i++) {
-            // 如果存款者存在，在数组中更新该存款者信息
-            if (msg.sender == contributors[i].addr) {
-                contributors[i].amount += msg.value;
-                isExisting = true;
-                break;
-            }
-        }
-        if (!isExisting) {
-            // 如果存款者不存在，在数组中新增该存款者信息
-            contributors.push(Contributor(msg.sender, msg.value));
+        _updateTop3();
+    }
+
+    function _updateTop3() private {
+        if (balances[msg.sender] > balances[top3[0]]) {
+            top3[2] = top3[1];
+            top3[1] = top3[0];
+            top3[0] = msg.sender;
+        } else if (balances[msg.sender] > balances[top3[1]]) {
+            top3[2] = top3[1];
+            top3[1] = msg.sender;
+        } else if (balances[msg.sender] > balances[top3[2]]) {
+            top3[2] = msg.sender;
         }
     }
 
@@ -44,31 +44,5 @@ contract Bank {
         if (!success) {
             revert("failed!");
         }
-    }
-
-    function getTop3() public view returns (Contributor[] memory) {
-        require(contributors.length > 0, "No contributors!");
-        // 对数组进行排序
-        Contributor[] memory sortedContributors = contributors;
-        for (uint i = 0; i < sortedContributors.length - 1; i++) {
-            for (uint j = 0; j < sortedContributors.length - i - 1; j++) {
-                if (
-                    sortedContributors[j].amount <
-                    sortedContributors[j + 1].amount
-                ) {
-                    Contributor memory temp = sortedContributors[j];
-                    sortedContributors[j] = sortedContributors[j + 1];
-                    sortedContributors[j + 1] = temp;
-                }
-            }
-        }
-        // 取出前三名（不足 3 名则全部返回）
-        Contributor[] memory topContributors = new Contributor[](3);
-        for (uint i = 0; i < 3; i++) {
-            if (i < sortedContributors.length) {
-                topContributors[i] = sortedContributors[i];
-            }
-        }
-        return topContributors;
     }
 }
